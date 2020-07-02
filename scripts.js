fbbca2943b1c147dfb93b35d59148a479cd6c161
Trezentos <gustavofagundes1998@hotmail.com>
2020-07-01 21:50:04 -0300

    Feito o frontend

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let btnQueroAjudar = document.querySelector('header button')
+
+btnQueroAjudar.addEventListener('click', function(){
+let form = document.querySelector('.form')
+
+    form.classList.toggle('hide')
+    
+})
